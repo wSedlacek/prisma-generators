ab78f479a61957d50a385f4ef4d4c1668381add0
ILshat Khamitov <admin@site15.ru>
2020-02-18 02:35:23 +0500

    fix: update action-resolver-class

--- src/generator/action-resolver-class.ts
+++ src/generator/action-resolver-class.ts
@@ -92,7 +92,7 @@ export default async function generateActionResolverClass(
             name: "ctx",
             // TODO: import custom `ContextType`
             type: "any",
-            decorators: [{ name: "Ctx", arguments: [] }],
+            decorators: [{ name: "Context", arguments: [] }],
           },
           ...(!argsTypeName
             ? []
